4aaea4b149170a62a838f00d8479fd307ea74f36
trgcghai <conghai.tpma@gmail.com>
2024-09-10 20:38:13 +0700

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,25 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
+
+export function Header() {
+    return <div className="bg-blue-500 py-2">
+        <div className="container mx-auto">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <NavigationMenuLink>Link</NavigationMenuLink>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+    </div>
+}
